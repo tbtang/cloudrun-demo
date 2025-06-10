29c856bf29593479000f5975ca83a7aea0dd2c2a
tbtang <63529375+tbtang@users.noreply.github.com>
2025-06-10 18:29:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def post():
     try:
-        return "<p>Hello, World!</p>"
+        return "<p>Hello, World! Hi, TCB RUN!</p>"
     except Exception as e:
         print("[E] request except", e)
 
